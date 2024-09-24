105a05d3e1abfd5f0320647c650e645265c43de8
rhsehfm33 <rhsehfm33@gmail.com>
2024-09-25 00:34:46 +0900

    feat 포인트 히스토리 조회 구현

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -33,7 +33,7 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        return pointService.getPointHistories(id);
     }
 
     /**
--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,7 +1,10 @@
 package io.hhplus.tdd.point;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 
@@ -9,8 +12,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PointService {
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
 
     public UserPoint getUserPoint(long userId) {
         return userPointTable.selectById(userId);
+    }
+
+    public List<PointHistory> getPointHistories(long userId) {
+        return pointHistoryTable.selectAllByUserId(userId);
     }
 }
